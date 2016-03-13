60dabe09366f5731f41cdcd2fb628b2e2900ed30
Andrey Okolotin <andrey.okolotin@akvelon.com>
2016-03-13 21:45:43 +0300

    Added webApi proj.

---
+++ FrankWebApi/FrankWebApi/Controllers/CommonController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace FrankWebApi.Controllers
+{
+    public class CommonController : ApiController
+    {
+        [Route("api/accountInfo")]
+        public IHttpActionResult GetAccountInfo()
+        {
+            return this.Ok("Info template");
+        }
+    }
+}
